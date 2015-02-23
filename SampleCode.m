aef64ede8b8fcc3d87003dd876d65d3c4b438afe
YuxinSun <yuxin.sun.13@ucl.ac.uk>
2015-02-23 18:08:32 +0000

    LPBoost for TCR Classification
    With dynamic programming feature selection if file names started with
    dynamic

---
+++ SampleCode.m
@@ -0,0 +1,61 @@
+%% Sample Code
+
+%% LPBoost
+[M, ~] = size(data);
+nu = 0.1:0.1:0.9;
+
+group = zeros(M, length(nu));
+acc = zeros(length(nu), 1);
+
+for i = 1 : M
+    data_new = data;
+    data_new(i, :) = [];
+    
+    label_new = label;
+    label_new(i) = [];
+    
+    for j = 1 : length(nu)
+        model = LPBoostYS(data_new,label_new,1/(nu(j)*(M-1)),16000);
+        
+        group(i, j) = sign(data(i, model.idx) * model.a');
+    end;
+end;
+
+for i = 1 : length(nu)
+    acc(i) = sum(group(:, i) == label)/M;
+end;
+
+%% Dynamic LPBoost (Fixed Length)
+
+% Set length
+l = 5;
+
+M = size(data, 1);
+miu = 0.1:0.1:0.9;
+
+group = zeros(M, length(miu));
+acc = zeros(length(miu), 1);
+
+for i = 1 : length(miu)
+    for j = 1 : M
+
+        label_new = label;
+        label_new(j) = [];
+        data_new = data;
+        data_new(j, :) = [];
+        
+        model = dynamicLPBoostUnnorm(data_new,label_new,1/((M-1)*miu(i)),l,16000);
+        
+        data_new = zeros(1, size(model.sub, 2));
+        for counter = 1 : size(model.sub, 2)
+            data_new(counter) = featSelectUnnorm(data(j, :), model.sub(:, counter),...
+                model.atr{counter}, length(model.sub(:, counter)));
+        end;
+        group(j, i) = sign(data_new * model.a');
+        
+    end;
+end;
+
+for i = 1 : length(miu)       
+    acc(i) = sum(group(:, i) == label)/M;
+end;
