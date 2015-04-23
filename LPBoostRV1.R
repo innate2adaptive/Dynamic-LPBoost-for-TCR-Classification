70646bba4a34b87d4c90de10b990da9a44444bc3
bchain <b.chain@ucl.ac.uk>
2015-04-23 17:33:24 +0100

    This is the first version of an R function for the LPBoost algorithm from Demirez, Bennett and Shawe-Taylor (2002). Based on Yuxin Sun's version in Matlab but uses lpSolve instead of cvx

---
+++ LPBoostRV1.R
@@ -0,0 +1,104 @@
+#function model for LP Boost in R
+## Original code  by: Dimitrios Athanasakis (dathanasakis [at] gotbim [dot] com)
+# and Yuxin Sun: yuxin.sun [at] ucl [dot] ac [dot] uk; using cvx package in Matlab
+#X is a matrix of weak learners; each row is a weak learner; each column is a sample. 
+#Y is a classification vector; this must correspond to the order of columns in X, and must be {1,-1}.
+#D is a weight for teh penalty ;it should be set at 1/mv where m is teh number of samples and v [0,1]  
+#iter is the number of iterations before stopping 
+#OUTPUT of function is the primal and dual variables 
+LPBoostYS <-function (X, Y, D, iter){
+#requires package lpSolve
+library (lpSolve)
+start_time<-proc.time()
+# Output:
+# model: structure variable of LPBoost model
+
+#Improved code
+# Original code by: Dimitrios Athanasakis (dathanasakis [at] gotbim [dot] com)
+# Yuxin Sun: yuxin.sun [at] ucl [dot] ac [dot] uk
+
+#[M, ~] = size(X);
+M<-dim(X)[1]
+
+# Initialise
+counter <- 1;
+a <- matrix(0,nrow=1,ncol=M)
+beta <- 0;
+u = rep(1,M)/M
+eps<-1e-6
+#calculate which triplet gives best discrimination by maximising uY*X
+uY<-u*Y
+weak_L<-uY %*% X
+val=max(weak_L)
+ind<-which.max(weak_L)
+index<-ind
+hypo<-as.matrix(X[,ind])
+#rm(u,beta)
+while (counter <= iter & val >= beta+eps)   {     
+  if (counter >1) {
+    hypo = cbind(hypo,X[, ind])
+  }
+  model<-LPcvx(hypo, Y, D)
+  
+  #model.beta(counter) = beta;
+  #model.idx(counter) = ind;
+  cat("\nITERATION ",counter,"\t beta",  beta, "\t Val", val)
+  counter <- counter + 1
+  u<-model[[1]]
+  beta<-model[[2]]
+  #Z <- apply (hypo, 2 , function(x){x*Y})
+  #recalculate which triplet gives best discrimination by maximising uY*X
+  uY<-u*Y
+  weak_L<-uY %*% X
+  val=max(weak_L)
+  ind<-which.max(weak_L)
+  index<-c(index,ind)
+}
+cat ("\n user system elapsed \n",proc.time()[1]-start_time[1],"  ",proc.time()[2:3]-start_time[2:3],"\n")
+#outputs both primal ("a") and dual "u" variables
+list(model[[1]],model[[3]],index)
+}
+
+#######################################################################################################################
+#this function does convex minimization to find the solution of the dual problem for the weak learner
+LPcvx <-function(X1,Y,D){ 
+# Input:
+#X: hypothesis space up to date
+# Y: label
+# D: regularisation parameter
+# Output:
+
+# u: misclassification cost
+# a: coefficients
+# betaa: objective
+#X1<-hypo
+
+M<-dim(X1)[1]
+N<-dim(X1)[2]
+Z <- apply (X1, 2 , function(x){x*Y})
+#these are teh constrains that sum less than beta
+
+beta.con<-cbind(t(Z),rep(-1,N))
+#constraint that sum u =1
+sumu.con<-c(rep(1,M),0)
+#constraint that all u < D
+UltD.con<-cbind(diag(M),rep(0,M))
+#combine these constraints 
+f.con<-rbind(beta.con,sumu.con,UltD.con)
+f.dir<-c(rep("<=",N),"==",rep("<=",M))
+f.rhs<-c(rep(0,N),1,rep(D,M))
+#beta<-100
+#trip<-c()
+
+#the objective function is to minimize beta, the last coefficient
+f.obj<-c(rep(0,M),1)
+LP<-lp("min",f.obj,f.con,f.dir,f.rhs,compute.sens=TRUE)
+#LP<-lp("min",f.obj,f.con,f.dir,f.rhs)
+beta<-LP$objval
+u<-LP$solution[1:M]
+#duals coefficients are before  dual constraints
+end <-dim(f.con)[1]+1
+
+list(u,beta,LP$duals)
+        }
+#a(a<=eps)=0;
